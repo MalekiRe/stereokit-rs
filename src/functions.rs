e84b5e30fa6f7b6e234ef5dd57ca1688ebd5f542
malek <pocmalek@gmail.com>
2022-07-05 18:14:48 -0700

    feat: sk_shutdown

--- src/functions.rs
+++ src/functions.rs
@@ -96,6 +96,12 @@ pub fn sk_init(settings: SKSettings2) -> bool {
 		return false;
 	}
 }
+pub fn sk_shutdown() {
+	unsafe {
+		stereokit_sys::sk_shutdown();
+	}
+}
+
 pub fn sk_run_data(on_update: &mut Box<&mut dyn FnMut()>, on_close: &mut Box<&mut dyn FnMut()>) {
 	let on_update_c_void: *mut c_void = on_update as *mut _ as *mut c_void;
 	let on_close_c_void: *mut c_void = on_close as *mut _ as *mut c_void;
