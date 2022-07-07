ff527ef8e1a2866df9c4555bcf1460dad3a9b7cb
Nova <technobaboo@gmail.com>
2022-07-07 00:30:57 -0400

    refactor(lib): allow unused

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,40 @@
+#[allow(unused)]
 pub mod constants;
+#[allow(unused)]
 pub mod enums;
-pub mod functions;
-pub mod structs;
-pub mod values;
-pub mod material;
-pub mod mesh;
-pub mod model;
-pub mod shader;
-pub mod texture;
-pub mod pose;
-pub mod textstyle;
+#[allow(unused)]
 pub mod font;
-pub mod ui;
-pub mod richtext;
+#[allow(unused)]
+pub mod functions;
+#[allow(unused)]
 pub mod input;
+#[allow(unused)]
+pub mod material;
+#[allow(unused)]
+pub mod mesh;
+#[allow(unused)]
+pub mod model;
+#[allow(unused)]
+pub mod pose;
+#[allow(unused)]
+pub mod richtext;
+#[allow(unused)]
+pub mod shader;
+#[allow(unused)]
+pub mod structs;
+#[allow(unused)]
+pub mod textstyle;
+#[allow(unused)]
+pub mod texture;
+#[allow(unused)]
+pub mod ui;
+#[allow(unused)]
+pub mod values;
+
+#[test]
+fn test() {
+	let settings = functions::SKSettingBuilder::default().build().unwrap();
+	functions::sk_init(settings);
+
+	// functions::sk_run_data(on_update: &mut Box<&mut dyn FnMut()>, on_close: &mut Box<&mut dyn FnMut()>);
+}
