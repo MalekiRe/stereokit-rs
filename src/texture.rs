9c8591255ec7d70e507a03b0c3ffc8a9d2944468
Nova <technobaboo@gmail.com>
2022-08-12 17:24:10 -0400

    feat(texture): create

--- src/texture.rs
+++ src/texture.rs
@@ -1,9 +1,105 @@
+#![allow(non_upper_case_globals)]
+
 use crate::render::SphericalHarmonics;
 use crate::values::{color128_from, color128_to, color32_from, color32_to, Color32};
 use crate::StereoKit;
+use bitflags::bitflags;
 use std::ffi::CString;
 use std::fmt::Error;
 use stereokit_sys::tex_t;
+
+/// What type of color information will the texture contain? A
+/// good default here is Rgba32.
+#[repr(u32)]
+pub enum TextureFormat {
+	/// A default zero value for TexFormat! Uninitialized formats
+	/// will get this value and **** **** up so you know to assign it
+	/// properly :)
+	None = 0,
+	/// Red/Green/Blue/Transparency data channels, at 8 bits
+	/// per-channel in sRGB color space. This is what you'll want most of
+	/// the time you're dealing with color images! Matches well with the
+	/// Color32 struct! If you're storing normals, rough/metal, or
+	/// anything else, use Rgba32Linear.
+	RGBA32 = 1,
+	/// Red/Green/Blue/Transparency data channels, at 8 bits
+	/// per-channel in linear color space. This is what you'll want most
+	/// of the time you're dealing with color data! Matches well with the
+	/// Color32 struct.
+	RGBA32Linear = 2,
+	BGRA32 = 3,
+	BGRA32Linear = 4,
+	RG11B10 = 5,
+	RGB10A2 = 6,
+	/// Red/Green/Blue/Transparency data channels, at 16 bits
+	/// per-channel! This is not common, but you might encounter it with
+	/// raw photos, or HDR images.
+	RGBA64 = 7, // TODO: remove during major version update
+	RGBA64S = 8,
+	RGBA64F = 9,
+	/// Red/Green/Blue/Transparency data channels at 32 bits
+	/// per-channel! Basically 4 floats per color, which is bonkers
+	/// expensive. Don't use this unless you know -exactly- what you're
+	/// doing.
+	RGBA128 = 10,
+	/// A single channel of data, with 8 bits per-pixel! This
+	/// can be great when you're only using one channel, and want to
+	/// reduce memory usage. Values in the shader are always 0.0-1.0.
+	R8 = 11,
+	/// A single channel of data, with 16 bits per-pixel! This
+	/// is a good format for height maps, since it stores a fair bit of
+	/// information in it. Values in the shader are always 0.0-1.0.
+	R16 = 12,
+	/// A single channel of data, with 32 bits per-pixel! This
+	/// basically treats each pixel as a generic float, so you can do all
+	/// sorts of strange and interesting things with this.
+	R32 = 13,
+	/// A depth data format, 24 bits for depth data, and 8 bits
+	/// to store stencil information! Stencil data can be used for things
+	/// like clipping effects, deferred rendering, or shadow effects.
+	DepthStencil = 14,
+	/// 32 bits of data per depth value! This is pretty detailed,
+	/// and is excellent for experiences that have a very far view
+	/// distance.
+	Depth32 = 15,
+	/// 16 bits of depth is not a lot, but it can be enough if
+	/// your far clipping plane is pretty close. If you're seeing lots of
+	/// flickering where two objects overlap, you either need to bring
+	/// your far clip in, or switch to 32/24 bit depth.
+	Depth16 = 16,
+}
+
+bitflags! {
+	/// Textures come in various types and flavors! These are bit-flags
+	/// that tell StereoKit what type of texture we want; and how the application
+	/// might use it!
+	pub struct TextureType: u32 {
+		/// A standard color image; without any generated mip-maps.
+		const ImageNoMips = 1 << 0;
+		/// A size sided texture that's used for things like skyboxes;
+		/// environment maps; and reflection probes. It behaves like a texture
+		/// array with 6 textures.
+		const Cubemap = 1 << 1;
+		/// This texture can be rendered to! This is great for textures
+		/// that might be passed in as a target to Renderer.Blit; or other
+		/// such situations.
+		const RenderTarget = 1 << 2;
+		/// This texture contains depth data; not color data!
+		const Depth = 1 << 3;
+		/// This texture will generate mip-maps any time the contents
+		/// change. Mip-maps are a list of textures that are each half the
+		/// size of the one before them! This is used to prevent textures from
+		/// 'sparkling' or aliasing in the distance.
+		const Mips = 1 << 4;
+		/// This texture's data will be updated frequently from the
+		/// CPU (not renders)! This ensures the graphics card stores it
+		/// someplace where writes are easy to do quickly.
+		const Dynamic = 1 << 5;
+		/// A standard color image that also generates mip-maps
+		/// automatically.
+		const Image = Self::ImageNoMips.bits | Self::Mips.bits;
+	}
+}
 
 pub struct Texture<'a> {
 	sk: &'a StereoKit<'a>,
@@ -16,6 +112,18 @@ impl Drop for Texture<'_> {
 	}
 }
 impl<'a> Texture<'a> {
+	pub fn create(
+		sk: &'a StereoKit,
+		texture_type: TextureType,
+		format: TextureFormat,
+	) -> Result<Self, Error> {
+		let tex: tex_t =
+			unsafe { stereokit_sys::tex_create(texture_type.bits().into(), format as u32) };
+		if tex.is_null() {
+			return Err(Error);
+		}
+		Ok(Texture { sk, tex })
+	}
 	pub fn from_color32(
 		sk: &'a StereoKit,
 		data: Color32,
@@ -27,13 +135,13 @@ impl<'a> Texture<'a> {
 		if uses_srgb_data {
 			my_var = 1;
 		}
-		let texture: tex_t = unsafe {
+		let tex: tex_t = unsafe {
 			stereokit_sys::tex_create_color32(&mut color32_from(data), width, height, my_var)
 		};
-		if texture.is_null() {
+		if tex.is_null() {
 			return Err(Error);
 		}
-		Ok(Texture { sk, tex: texture })
+		Ok(Texture { sk, tex })
 	}
 
 	pub fn from_cubemap_equirectangular(
@@ -54,6 +162,9 @@ impl<'a> Texture<'a> {
 				load_priority,
 			)
 		};
+		if tex.is_null() {
+			return Err(Error);
+		}
 		Ok((
 			Texture { sk, tex },
 			SphericalHarmonics {
