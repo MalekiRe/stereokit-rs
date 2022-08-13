7b08afb2365f4e656143ec97f24b67441b927e48
Nova <technobaboo@gmail.com>
2022-08-13 15:15:03 -0400

    refactor(ui): move settings outside its own module

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,5 +1,11 @@
-pub mod ui;
 pub mod window;
-
-pub use ui::*;
 pub use window::*;
+
+use stereokit_sys::{bool32_t, text_align_, ui_label, ui_sameline, ui_settings, ui_space, ui_text};
+
+pub type UISettings = stereokit_sys::ui_settings_t;
+pub fn settings(settings: UISettings) {
+	unsafe {
+		ui_settings(settings);
+	}
+}
--- src/ui/ui.rs
+++
@@ -1,8 +0,0 @@
-use stereokit_sys::{bool32_t, text_align_, ui_label, ui_sameline, ui_settings, ui_space, ui_text};
-
-pub type UISettings = stereokit_sys::ui_settings_t;
-pub fn settings(settings: UISettings) {
-	unsafe {
-		ui_settings(settings);
-	}
-}
