504e3304a40928201de9e6bcd55323e058c9c915
Nova <technobaboo@gmail.com>
2022-08-18 09:15:34 -0400

    refactor(material): remove individual material parameter type fns

--- src/material.rs
+++ src/material.rs
@@ -115,49 +115,6 @@ impl Material {
 	pub fn get_queue_offset(&self) -> i32 {
 		unimplemented!()
 	}
-	pub fn set_float(&self, name: &str, value: f32) {
-		let c_str = CString::new(name).unwrap();
-		unsafe { material_set_float(self.material.as_ptr(), c_str.as_ptr(), value) }
-	}
-	pub fn set_vector2(&self, name: &str, value: Vec2) {
-		unimplemented!()
-	}
-	pub fn set_vector3(&self, name: &str, value: Vec3) {
-		unimplemented!()
-	}
-	pub fn set_color(&self, name: &str, value: Color128) {
-		unimplemented!()
-	}
-	pub fn set_vector4(&self, name: &str, value: Vec4) {
-		unimplemented!()
-	}
-	pub fn set_vector(&self, name: &str, value: Vec4) {
-		self.set_vector4(name, value);
-	}
-	pub fn set_int(&self, name: &str, value: i32) {
-		unimplemented!()
-	}
-	pub fn set_int2(&self, name: &str, value1: i32, value2: i32) {
-		unimplemented!()
-	}
-	pub fn set_int3(&self, name: &str, value1: i32, value2: i32, value3: i32) {
-		unimplemented!()
-	}
-	pub fn set_int4(&self, name: &str, value1: i32, value2: i32, value3: i32, value4: i32) {
-		unimplemented!()
-	}
-	pub fn set_matrix(&self, name: &str, value: Matrix) {
-		unimplemented!()
-	}
-	pub fn set_texture(&self, name: &str, value: Texture) {
-		let c_str = CString::new(name).unwrap();
-		unsafe {
-			material_set_texture(self.material.as_ptr(), c_str.as_ptr(), value.tex.as_ptr());
-		}
-	}
-	pub fn set_texture_id(&self, id: u64, value: Texture) -> bool {
-		unimplemented!()
-	}
 	pub fn has_parameter(&self, name: &str, type_: impl MaterialParameter) -> bool {
 		unimplemented!()
 	}
