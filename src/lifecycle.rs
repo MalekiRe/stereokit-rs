609357c934292d513aecf25c3b72b30aa1cf6543
Nova <technobaboo@gmail.com>
2022-08-19 16:25:35 -0400

    feat(settings): disable_desktop_input_window

--- src/lifecycle.rs
+++ src/lifecycle.rs
@@ -65,6 +65,7 @@ pub struct SKSettingsBuilt {
 	flatscreen_width: u32,
 	flatscreen_height: u32,
 	disable_flatscreen_mr_sim: bool,
+	disable_desktop_input_window: bool,
 	disable_unfocused_sleep: bool,
 }
 
@@ -98,6 +99,8 @@ impl Settings {
 			flatscreen_width: self.flatscreen_width.unwrap_or_default() as i32,
 			flatscreen_height: self.flatscreen_height.unwrap_or_default() as i32,
 			disable_flatscreen_mr_sim: self.disable_flatscreen_mr_sim.unwrap_or_default()
+				as bool32_t,
+			disable_desktop_input_window: self.disable_desktop_input_window.unwrap_or_default()
 				as bool32_t,
 			disable_unfocused_sleep: self.disable_unfocused_sleep.unwrap_or_default() as bool32_t,
 			android_java_vm: ptr::null_mut(),
@@ -146,80 +149,84 @@ type PanicPayload = Box<dyn Any + Send + 'static>;
 /// panic-poisoned, and the panic should likely be propagated.
 unsafe extern "C" fn callback_trampoline<F, LST, GST>(payload_ptr: *mut c_void)
 where
-    F: FnMut(&mut LST, &GST),
+	F: FnMut(&mut LST, &GST),
 {
-    let payload =
-        &mut *(payload_ptr as *mut (&mut F, &mut LST, &GST, &mut Option<PanicPayload>));
-    let (closure, state, global_state, caught_panic) = payload;
+	let payload = &mut *(payload_ptr as *mut (&mut F, &mut LST, &GST, &mut Option<PanicPayload>));
+	let (closure, state, global_state, caught_panic) = payload;
 
-    if caught_panic.is_some() {
-        // we should consider the state poisoned and not run the callback
-        return;
-    }
+	if caught_panic.is_some() {
+		// we should consider the state poisoned and not run the callback
+		return;
+	}
 
-    // the caller should ensure closure variables and state cannot be observed
-    // after the panic without catching the panic,
-    // which will in turn require them to be UnwindSafe
-    let mut closure = AssertUnwindSafe(closure);
-    let mut state = AssertUnwindSafe(state);
-    // TODO: is global state always safe to be re-observed after a shutdown?
-    let mut global_state = AssertUnwindSafe(global_state);
+	// the caller should ensure closure variables and state cannot be observed
+	// after the panic without catching the panic,
+	// which will in turn require them to be UnwindSafe
+	let mut closure = AssertUnwindSafe(closure);
+	let mut state = AssertUnwindSafe(state);
+	// TODO: is global state always safe to be re-observed after a shutdown?
+	let mut global_state = AssertUnwindSafe(global_state);
 
-    let result = std::panic::catch_unwind(move || closure(*state, *global_state));
-    if let Err(panic_payload) = result {
-        caught_panic.replace(panic_payload);
-        stereokit_sys::sk_quit();
-    }
+	let result = std::panic::catch_unwind(move || closure(*state, *global_state));
+	if let Err(panic_payload) = result {
+		caught_panic.replace(panic_payload);
+		stereokit_sys::sk_quit();
+	}
 }
 
 impl StereoKit {
 	pub fn run(&self, mut on_update: impl FnMut(&DrawContext), mut on_close: impl FnMut()) {
 		self._run(&mut (), |(), sks| on_update(sks), |_, _| on_close());
 	}
-	pub fn run_stateful<ST>(&self, state: &mut ST, mut on_update: impl FnMut(&mut ST, &DrawContext), mut on_close: impl FnMut(&mut ST)) {
+	pub fn run_stateful<ST>(
+		&self,
+		state: &mut ST,
+		mut on_update: impl FnMut(&mut ST, &DrawContext),
+		mut on_close: impl FnMut(&mut ST),
+	) {
 		self._run(state, |st, sks| on_update(st, sks), |st, _| on_close(st));
 	}
 
-    fn _run<ST, U, S>(&self, state: &mut ST, mut update: U, mut shutdown: S)
-    where
-        U: FnMut(&mut ST, &DrawContext),
-        S: FnMut(&mut ST, &DrawContext),
-    {
+	fn _run<ST, U, S>(&self, state: &mut ST, mut update: U, mut shutdown: S)
+	where
+		U: FnMut(&mut ST, &DrawContext),
+		S: FnMut(&mut ST, &DrawContext),
+	{
 		let draw_context = DrawContext(PhantomData);
 
-        // use one variable so shutdown doesn't run if update panics
-        let mut caught_panic = Option::<PanicPayload>::None;
+		// use one variable so shutdown doesn't run if update panics
+		let mut caught_panic = Option::<PanicPayload>::None;
 
-        let mut update_ref: (&mut U, &mut ST, &DrawContext, &mut Option<PanicPayload>) =
-            (&mut update, state, &draw_context, &mut caught_panic);
-        let update_raw = &mut update_ref
-            as *mut (&mut U, &mut ST, &DrawContext, &mut Option<PanicPayload>)
-            as *mut c_void;
+		let mut update_ref: (&mut U, &mut ST, &DrawContext, &mut Option<PanicPayload>) =
+			(&mut update, state, &draw_context, &mut caught_panic);
+		let update_raw = &mut update_ref
+			as *mut (&mut U, &mut ST, &DrawContext, &mut Option<PanicPayload>)
+			as *mut c_void;
 
-        let mut shutdown_ref: (&mut S, &mut ST, &DrawContext, &mut Option<PanicPayload>) =
-            (&mut shutdown, state, &draw_context, &mut caught_panic);
-        let shutdown_raw = &mut shutdown_ref
-            as *mut (&mut S, &mut ST, &DrawContext, &mut Option<PanicPayload>)
-            as *mut c_void;
+		let mut shutdown_ref: (&mut S, &mut ST, &DrawContext, &mut Option<PanicPayload>) =
+			(&mut shutdown, state, &draw_context, &mut caught_panic);
+		let shutdown_raw = &mut shutdown_ref
+			as *mut (&mut S, &mut ST, &DrawContext, &mut Option<PanicPayload>)
+			as *mut c_void;
 
 		if self.ran.set(()).is_err() {
 			return;
 		}
 
-        unsafe {
-            stereokit_sys::sk_run_data(
-                Some(callback_trampoline::<U, ST, DrawContext>),
-                update_raw,
-                Some(callback_trampoline::<S, ST, DrawContext>),
-                shutdown_raw,
-            );
-        }
+		unsafe {
+			stereokit_sys::sk_run_data(
+				Some(callback_trampoline::<U, ST, DrawContext>),
+				update_raw,
+				Some(callback_trampoline::<S, ST, DrawContext>),
+				shutdown_raw,
+			);
+		}
 
-        if let Some(panic_payload) = caught_panic {
-            std::panic::resume_unwind(panic_payload);
-        }
-    }
-	
+		if let Some(panic_payload) = caught_panic {
+			std::panic::resume_unwind(panic_payload);
+		}
+	}
+
 	pub fn quit(&self) {
 		unsafe { stereokit_sys::sk_quit() };
 	}
